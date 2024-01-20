577a4e5cff037ebb8128942bee4e4090124d673b
Andrii7571 <felixking43@gmail.com>
2024-01-21 01:09:37 +0300

    1

---
+++ js/01-gallery.js
@@ -0,0 +1,37 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+// 1. Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+const gallery = document.querySelector('.gallery');
+
+
+
+const markup = galleryItems.map(({ description, original, preview }) => {
+    return `<li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img class="gallery__image" data-source="${original}" src="${preview}" alt="${description}"/>
+                </a>
+            </li>`
+}).join('');
+ 
+gallery.insertAdjacentHTML('beforeend', markup);
+
+// 2.Реалізація делегування на ul.gallery і отримання url великого зображення.
+gallery.addEventListener('click', onClick);
+
+function onClick(evt) {
+    evt.preventDefault();
+    const instance = basicLightbox.create(
+        `<img class="gallery__image" src="${evt.target.dataset.source}" alt="${evt.target.currentSrc}"/>`
+    );
+
+    instance.show();
+
+    gallery.addEventListener('keydown', offModal);
+
+    function offModal(evt) {
+        if (evt.key === 'Escape') {
+            instance.close();
+    }};
+};
+
